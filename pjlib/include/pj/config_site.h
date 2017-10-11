d19785202958928bb87898b2e803a0b9b38d946b
fhsiao <frank.hsiao@hotmail.com>
2017-10-11 12:14:10 -0600

    APPS-179: Add config_site.h

---
+++ pjlib/include/pj/config_site.h
@@ -0,0 +1,5 @@
+#define PJ_CONFIG_ANDROID               1
+#include <pj/config_site_sample.h>
+#   define PJ_HAS_IFADDRS_H             0
+#	define PJMEDIA_HAS_SPEEX_AEC		0
+#	define PJMEDIA_HAS_SPEEX_CODEC		0
