0260fd545d23b2fd7fdf345fdf7e6614a7cdd9e5
fhsiao <frank.hsiao@hotmail.com>
2017-11-13 13:52:34 -0700

    APPS-339: Increase allowable packet size.
    Defaults to 4000 in sip_config.h if not defined.
    Needed for complicated sites.  Increase max logging
    size to agree with the packet size.

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -4,3 +4,9 @@
 #	define PJMEDIA_HAS_SPEEX_CODEC		0
 #   define PJ_HAS_SSL_SOCK              1
 #   define PJLIB_USE_SLNK_TIMING        1
+
+/* Increase allowable packet size.  Defaults to 4000 in sip_config.h
+ *  if not defined.  Needed for complicated sites.  Increase max logging
+ *  size to agree with the packet size */
+#   define PJSIP_MAX_PKT_LEN            6000
+#   define PJ_LOG_MAX_SIZE              (PJSIP_MAX_PKT_LEN+500)
